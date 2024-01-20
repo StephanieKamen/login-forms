f809529bc6c523d8eeaebe0a5712773716f9ee4f
Stephanie Kamen <st.kamen@gmx.de>
2024-01-20 08:53:43 +0100

    Added JS to alert the value of the individual given password when submitting

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Project under constuction 😉")
+// Challenge 1: Alert  value of password input when submitting
+
+function showPassword(event) {
+    event.preventDefault();
+    let passwordInput = document.querySelector("#password-input");
+    alert(`Your password is ${passwordInput.value}`);
+}
+
+let passwordForm = document.querySelector("#password-form");
+passwordForm.addEventListener("submit", showPassword);
+
+
+// Challenge 2: Alert  value of username and email inputs when submitting
+
+
